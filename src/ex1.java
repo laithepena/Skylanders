f99de361eb81b4e127728a4ecc49d2cdbc3fd7dc
shivadeep <laithepena@gmail.com>
2017-01-05 16:33:37 -0800

    Signed-off-by: shivadeep <laithepena@gmail.com>

--- src/ex1.java
+++ src/ex1.java
@@ -1,4 +1,65 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class ex1 {
+	
+
+
+	public static void main(String[] args) {
+
+		String s="abc"; 
+		//String a=funcStringPerm(s.toCharArray(),0,s.length()-1);
+		Set<String> s1=new HashSet<String>();
+		
+		funcStringPerm(s.toCharArray(),0,s.length()-1,s1);
+		
+		for(String ss:s1){
+			System.out.println(ss);
+		}
+		
+	}
+
+	private static void funcStringPerm(char[] c,int i,int n,Set s1) {
+
+		//Set<String> s1=new HashSet<String>();
+		if(i==n){
+			//System.out.print("##########  ");
+			//System.out.println(c);
+			// return String.valueOf(c);
+            s1.add(String.valueOf(c));
+
+		}
+
+
+		else{
+			for(int j=i;j<=n;j++){
+				//swap(i,j)
+				char temp=c[i];
+				c[i]=c[j];
+				c[j]=temp;
+				/*System.out.println(c);
+				System.out.println("End --------------");*/
+				funcStringPerm(c,i+1,n,s1);
+
+				//swap(i,j);
+				char temp1=c[i];
+				c[i]=c[j];
+				c[j]=temp1;
+				/*System.out.println(c);
+				System.out.println("End AAAAAAAAAAAAAAAAAAAA");*/
+			}
+
+
+
+		}
+
+
+
+
+	} 
+
+
+
+
 
 }
